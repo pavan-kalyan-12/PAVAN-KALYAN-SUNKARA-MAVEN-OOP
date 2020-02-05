03e1de48680d8ceeaedf84ed2ee49a03268dc03a
pavan-kalyan-12 <s.pavankalyanayyappa@gmail.com>
2020-02-05 11:21:53 +0530

    Initial Commit

---
+++ epam/src/main/java/com/pavan/epam/Candy.java
@@ -0,0 +1,12 @@
+package com.pavan.epam;
+
+
+public class Candy extends Sweet{
+	String Name;
+	int cost;
+	float weight;
+	int sweetness;
+	public Candy(String Name,int cost,float weight,int sweetness) {
+		super(Name,cost,weight,sweetness);
+	}
+}
